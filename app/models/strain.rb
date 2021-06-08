ce5c92c7d7b9c959cb7c5062b5e36dfeaf95983f
Fabian <fpinoperez@outlook.cl>
2021-06-07 20:29:00 -0400

    Add testing to model strains.rb

--- app/models/strain.rb
+++ app/models/strain.rb
@@ -1,7 +1,7 @@
 class Strain < ApplicationRecord
     has_many :wine_strains
     has_many :wines, through: :wine_strains, dependent: :destroy
-
+    validates :name, presence: true, uniqueness: true
 
     def availability
         if available == true
